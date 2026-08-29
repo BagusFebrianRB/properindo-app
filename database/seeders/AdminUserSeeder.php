fb839d9f7e659a9398bf0339049b0f91fbf6433e
BagusFebrianRB <bagusfebrian67@gmail.com>
2026-08-29 17:17:10 +0700

    add seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => 'admin@properindo.test'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password123'),
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'bagus@properindo.test'],
+            [
+                'name' => 'Bagus',
+                'password' => Hash::make('password123'),
+            ]
+        );
+    }
+}
